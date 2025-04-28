0e56b67a7e7c668d06400625850dd67f82a240f6
eSheppardKHS <ethansheppardsfitbit@gmail.com>
2025-04-28 13:07:21 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -100,14 +100,14 @@ var init = function (window) {
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if (circle.x > canvas.width ){
+            if (circle.x > canvas.width) {
                 circle.x = 0;
-            } else if (circle.x < 0 ){
+            } else if (circle.x < 0) {
                 circle.x = canvas.width;
             }
-            if (circle.y > canvas.height){
+            if (circle.y > canvas.height) {
                 circle.y = 0;
-            } else if (circle.y < 0){
+            } else if (circle.y < 0) {
                 circle.y = canvas.height;
             }
             
